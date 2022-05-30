75cb23cb07bae17c4cf985d759506292e1c2eac0
Aleksey K <aleksey.kabishau@icloud.com>
2022-05-30 18:35:18 -0400

    feat: configure search options vc to use custom table view cell

--- FlickrAPI/Controller/SearchOptionsVC.swift
+++ FlickrAPI/Controller/SearchOptionsVC.swift
@@ -8,10 +8,73 @@
 import UIKit
 
 class SearchOptionsVC: UIViewController {
+	
+	let availableOptions = ["Interested Photos", "Search by Name"]
+	
+	let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-		view.backgroundColor = .systemBackground
+		configure()
     }
+	
+	
+	private func configure() {
+		view.backgroundColor = .systemBackground
+		title = "Search Options"
+		
+		view.addSubview(tableView)
+		tableView.translatesAutoresizingMaskIntoConstraints = false
+		tableView.rowHeight = 80
+		tableView.register(SearchTypeCell.self, forCellReuseIdentifier: SearchTypeCell.reuseId)
+		
+		tableView.dataSource = self
+		tableView.delegate = self
+		
+		NSLayoutConstraint.activate([
+			tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			tableView.topAnchor.constraint(equalTo: view.topAnchor),
+			tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+		])
+		
+	}
+}
+
+
+extension SearchOptionsVC: UITableViewDataSource {
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return availableOptions.count
+	}
+	
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		
+		let searchName = availableOptions[indexPath.row]
+		
+		let cell = tableView.dequeueReusableCell(withIdentifier: SearchTypeCell.reuseId, for: indexPath) as! SearchTypeCell
+		
+		cell.configure(for: searchName)
+		
+		//cell.iconImageView.image = UIImage(systemName: "magnifyingglass.circle")
+		//cell.titleLabel.text = searchName
+		
+		return cell
+	}
+}
+
+
+extension SearchOptionsVC: UITableViewDelegate {
+	
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		print(#function)
+		tableView.deselectRow(at: indexPath, animated: true)
+		
+		let cell = tableView.cellForRow(at: indexPath) as! SearchTypeCell
+		//cell.saved.toggle()
+		print("\(cell.title) is selected")
+		
+	}
 }
---
+++ FlickrAPI/View/SearchTypeCell.swift
@@ -0,0 +1,79 @@
+//
+//  SearchTypeCell.swift
+//  FlickrAPI
+//
+//  Created by Aleksey on 5/30/22.
+//
+
+import UIKit
+
+class SearchTypeCell: UITableViewCell {
+	
+	static let reuseId = String(describing: SearchTypeCell.self)
+	
+	var title: String = "" {
+		didSet {
+			titleLabel.text = title
+		}
+	}
+	
+	var saved: Bool = false {
+		didSet {
+			if saved {
+				button.setImage(.checkmark, for: .normal)
+			} else {
+				button.setImage(.add, for: .normal)
+			}
+		}
+	}
+
+	
+	let titleLabel = UILabel()
+	let button = UIButton()
+
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		
+		configure()
+	}
+	
+	required init?(coder: NSCoder) { fatalError() }
+	
+	
+	func configure(for title: String) {
+		self.title = title
+		self.saved = false
+	}
+	
+	
+	@objc func buttonTapped(_ sender: UIButton) {
+		print(#function)
+	}
+	
+	private func configure() {
+		
+		//accessoryType = .detailDisclosureButton
+		
+		contentView.addSubview(titleLabel)
+		contentView.addSubview(button)
+		
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		button.translatesAutoresizingMaskIntoConstraints = false
+		
+		button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+		
+		NSLayoutConstraint.activate([
+			button.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20),
+			button.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.5),
+			button.widthAnchor.constraint(equalTo: button.heightAnchor),
+			button.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+			
+			titleLabel.leadingAnchor.constraint(equalTo: button.trailingAnchor, constant: 20),
+			titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+			titleLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+			
+		])
+		
+	}
+
+}
