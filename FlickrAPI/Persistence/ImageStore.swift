e2048240485357f5a82377b13f2a33b6054bb251
Aleksey K <aleksey.kabishau@icloud.com>
2022-06-04 21:21:07 -0400

    feat: add saving image on disk to persist between between app launches

--- FlickrAPI/Persistence/ImageStore.swift
+++ FlickrAPI/Persistence/ImageStore.swift
@@ -19,29 +19,52 @@ class CacheImageStore: ImageStore {
 	
 	private let cache = NSCache<NSString, UIImage>()
 	
+	let fileManager = FileManager.default
+	
 	
 	func saveImage(_ image: UIImage, forKey key: String) {
 		let cacheKey = key as NSString
 		cache.setObject(image, forKey: cacheKey)
 		print("just saved image in cache")
+		
+		let url = imageURL(forKey: key)
+		
+		if let data = image.jpegData(compressionQuality: 0.5) {
+			try? data.write(to: url)
+			print("just saved image on disk")
+		}
+		
 	}
 	
 	
 	func getImage(forKey key: String) -> UIImage? {
 		let cacheKey = key as NSString
 		
-		if let image = cache.object(forKey: cacheKey) {
+		if let imageFromCache = cache.object(forKey: cacheKey) {
 			print("got image from cache")
-			return image
-		} else {
-			print("no image - returning nil")
+			return imageFromCache
+		}
+		
+		let url = imageURL(forKey: key)
+		guard let imageFromDisk = UIImage(contentsOfFile: url.path) else {
+			print("no image on disk - nil")
 			return nil
 		}
+		
+		print("got image from disk - \(imageFromDisk)")
+		cache.setObject(imageFromDisk, forKey: cacheKey)
+		return imageFromDisk
 	}
 	
 	
 	func deleleImage(forKey key: String) {
 		let cacheKey = key as NSString
 		cache.removeObject(forKey: cacheKey)
+	}
+	
+	
+	private func imageURL(forKey key: String) -> URL {
+		let documentDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+		return documentDirectory.appendingPathComponent(key)
 	}
 }
